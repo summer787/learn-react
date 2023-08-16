1471cd4faa6b6b40d21180b6f881a946c6c6f98a
leesy <leesy787@gmail.com>
2023-08-16 15:08:38 +0900

    숫자 유형의 데이터 상태 관리
    - count
    - step
    - currentYear
    폼 필드의 value, defaultValue, readOnly, onChange
    상태와 파생 상태
    - lastName
    - firstName
    - fullName
    마우스 위치를 개별적으로 숫자 값 상태 관리
    객체 데이터 상태 관리
    - 중복된 데이터는 관리가 어렵다.
    -상태 초깃값은 변수로 관리하면 편리합니다.

--- src/layout/FooterBar.jsx
+++ src/layout/FooterBar.jsx
@@ -1,11 +1,14 @@
-
+import { useState } from "react";
 
 function FooterBar() {
+  // 현재(오늘) 년도를 화면에 출력하는 상태를 설정
+  const [currentYear] = useState(() => new Date().getFullYear());
+
   return (
     <footer className="p-5 grid place-content-center bg-slate-200">
       <small className="text-base text-slate-700">
-        모든 저작권은 이듬(EUID)에 있습니다.
-        </small>
+        Copyright {currentYear} &copy; 모든 저작권은 이듬(EUID)에 있습니다.
+      </small>
     </footer>
   );
 }
