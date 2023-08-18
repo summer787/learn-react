f1793627c66ee29a8361483818f96c9bed46c48a
leesy <leesy787@gmail.com>
2023-08-18 15:43:41 +0900

    수업 9일차 PocketBase 이미지 URL을 처리하는 방법 useEffect 훅 그리고 통신 중단(AbortController) 로직 재사용을 목적으로 커스텀 훅 useMouse 커스텀 훅 useMouse 훅 그리고 구독과 취소(클린업) React Router 시작하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,82 @@
+import { createBrowserRouter, RouterProvider, NavLink } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
-import LearnStateAndEffects from './pages/State_NumberType';
+
+const Nav = () => (
+  <nav>
+    <ul className="flex gap-4 p-5 font-extralight bg-slate-50/50 shadow-lg">
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'font-semibold text-rose-600' : ''
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/products"
+          className={({ isActive }) =>
+            isActive ? 'font-semibold text-rose-600' : ''
+          }
+        >
+          Products
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? 'font-semibold text-rose-600' : ''
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  </nav>
+);
+
+const Home = () => (
+  <div>
+    <Nav />
+    <h1 className="text-emerald-500">Home</h1>
+  </div>
+);
+const Products = () => (
+  <div>
+    <Nav />
+    <h1 className="text-emerald-500">Products</h1>
+  </div>
+);
+const Contact = () => (
+  <div>
+    <Nav />
+    <h1 className="text-emerald-500">Contact</h1>
+  </div>
+);
+
+// Router Object
+// 경로(path), 요소(element ← <Component />)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    chlidren: [
+      { index: true, element: <Home /> },
+      { path: 'products', element: <Products /> },
+      { path: 'contact', element: <Contact /> },
+    ],
+  },
+]);
+
+console.log(router);
 
 function App() {
   return (
     <div className="App">
-      <RootLayout>
-        <LearnStateAndEffects />
-      </RootLayout>
+      <RouterProvider router={router} />
     </div>
   );
 }
